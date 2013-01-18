d9e4b270805aa4189cb0f3bbfbd94a8d3cbf12dd
Paul Zabelin <blazingpair+paul@blazingcloud.net>
2013-01-18 12:07:42 -0800

    add image matcher

---
+++ Classes/Kiwi/KWImageMatcher.h
@@ -0,0 +1,12 @@
+//
+//  KWImageMatcher.h
+//
+//  Created by Blazing Pair on 12/10/12.
+//  Copyright (c) 2012 Blazing Cloud, Inc. All rights reserved.
+//
+
+#import "KWMatcher.h"
+
+@interface KWImageMatcher : KWMatcher
+- (void)matchImage:(NSString *)expectedImageName;
+@end
